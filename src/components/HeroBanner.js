8df49cf93806e2769ed6c11e90bcc4e58e2aa4c8
ashish sood <ashishsood1998@gmail.com>
2026-05-28 10:02:34 -0500

    hook used to get slides

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,7 +1,20 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-function HeroBanner (){
+function HeroBanner () {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const moveSlide = (direction) => {
+    setCurrentSlide((prev) => (prev + direction + slides.length) % slides.length);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      moveSlide(1);
+    }, 8000);
+
+    return () => clearInterval(interval);
+  }, []);
+
     return(
         <></>
     )
