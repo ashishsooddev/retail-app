a8500f3cc2157b71bfbe692c376527a3c75b3906
Manvir kaur <manvirkaur8193@gmail.com>
2026-05-28 11:05:19 -0500

    added quantity of items

--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -5,22 +5,31 @@ export const CartContext = createContext();
 export function CartProvider({ children }) {
   const [cart, setCart] = useState([]);
 
+
+
   function removeFromCart(id) {
     const newCart = cart.filter((item) => item.id !== id);
     setCart(newCart);
   }
 
+  function increaseQty(id) {
+    const newCart = cart.map((item) =>
+      item.id === id
+        ? { ...item, quantity: item.quantity + 1 } : item
+    );
+    setCart(newCart);
+  }
+
+  function decreaseQty(id) {
+    const newCart = cart.map((item) =>
+      item.id === id && item.quantity > 1
+        ? { ...item, quantity: item.quantity - 1 } : item
+    );
+    setCart(newCart);
+  }
+
   return (
-    <CartContext.Provider
-      value={{
-        cart,
-        addToCart,
-        removeFromCart,
-        increaseQty,
-        decreaseQty,
-      }}>
-      {children}
-    </CartContext.Provider>
+    <></>
   );
 }
 export default CartContext;
