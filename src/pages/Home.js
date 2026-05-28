2aa51e8f3fb7ae76fbb04476af92d96189f5f3e4
ashish sood <ashishsood1998@gmail.com>
2026-05-28 11:22:20 -0500

    skeletol imports added in Home-js

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import { useEffect, useMemo, useReducer, useState } from "react";
+import { motion } from "framer-motion";
+import { getCategories, getProducts } from "../api/api";
+import Header from "../components/Header";
+import HeroBanner from "../components/HeroBanner";
+import SecondaryBanner from "../components/SecondaryBanner";
+import Filter from "../components/Filter";
+import Sort from "../components/Sort";
+import ProductCard from "../components/ProductCard";
+
+function Home() {
+  return (
+<></>
+  );
+}
+
+export default Home;
