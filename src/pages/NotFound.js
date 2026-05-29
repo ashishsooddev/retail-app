11219e9427134d70b2d6ec2350e0a11c0d551245
ashish sood <ashishsood1998@gmail.com>
2026-05-29 11:57:31 -0500

    not found css upd

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -5,31 +5,41 @@ import Footer from "../components/Footer";
 
 function NotFound() {
   return (
-    <section className="notfound-container">
-       <Header/>
-      <div className="notfound-content">
-        <p className="error-text">ERROR 404</p>
-        <h1>Oops! This page is <br /> out of stock.</h1>
+    <>
+      <Header />
+      <section className="notfound-container">
+        <div className="notfound-content">
+          <p className="error-text">ERROR 404</p>
 
-        <p className="description">
-          The page you are looking for does not exist or has been
-          moved to a new collection. Let’s get you back to the
-          latest trends.
-        </p>
-        <div className="buttons">
-          <Link to="/" className="home-btn">RETURN TO HOME</Link>
-          <button className="browse-btn">BROWSE COLLECTIONS</button>
-        </div>
-      </div>
+          <h1>
+            Oops! This page is <br /> out of stock.
+          </h1>
 
-      <div className="image-section">
-        <div className="error-box">
-          <h1>404</h1>
+          <p className="description">
+            The page you are looking for does not exist or has been
+            moved to a new collection. Let’s get you back to the
+            latest trends.
+          </p>
+
+          <div className="buttons">
+            <Link to="/" className="home-btn">
+              RETURN TO HOME
+            </Link>
+
+            <button className="browse-btn">
+              BROWSE COLLECTIONS
+            </button>
+          </div>
         </div>
-      </div>
-      <Footer/>
-    </section>
+
+        <div className="image-section">
+          <div className="error-box">
+            <h1>404</h1>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
   );
 }
-
 export default NotFound;
