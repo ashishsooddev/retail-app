f35a8d2a462afec78f8ed7ecaf84eae4e156e9dd
ashish sood <ashishsood1998@gmail.com>
2026-05-29 11:30:42 -0500

    issues in not found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,32 +1,34 @@
 import { Link } from "react-router-dom";
 
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
 function NotFound() {
   return (
-     <section className="notfound-container">
+    <section className="notfound-container">
+       <Header/>
       <div className="notfound-content">
         <p className="error-text">ERROR 404</p>
         <h1>Oops! This page is <br /> out of stock.</h1>
 
-      <p className="description">
-        The page you are looking for does not exist or has been
-        moved to a new collection. Let’s get you back to the
-        latest trends.
-      </p>
+        <p className="description">
+          The page you are looking for does not exist or has been
+          moved to a new collection. Let’s get you back to the
+          latest trends.
+        </p>
+        <div className="buttons">
+          <Link to="/" className="home-btn">RETURN TO HOME</Link>
+          <button className="browse-btn">BROWSE COLLECTIONS</button>
+        </div>
+      </div>
 
-    <div className="buttons">
-    <Link to="/" className="home-btn">RETURN TO HOME</Link>
-
-    <button className="browse-btn">BROWSE COLLECTIONS</button>
-    </div>
-  </div>
-
-  <div className="image-section">
-    <div className="error-box">
-    <h1>404</h1>
-  </div>
-
-</div>
-</section>
+      <div className="image-section">
+        <div className="error-box">
+          <h1>404</h1>
+        </div>
+      </div>
+      <Footer/>
+    </section>
   );
 }
 
