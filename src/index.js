e463237356ff2b211dfbf537749942b0006aead3
ashish sood <ashishsood1998@gmail.com>
2026-05-29 09:40:36 -0500

    css file fixxed and added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from "react-router-dom";
 import './Style/cart.css';
+import './Style/home.css';
 import App from './App';
 
 import './Style/index.css';
