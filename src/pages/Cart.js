b50eeca5223f84c390c685f270395cbd3ca6bd63
Manvir kaur <manvirkaur8193@gmail.com>
2026-05-28 19:14:39 -0500

    fix cart.js

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,26 +1,27 @@
 import { CartContext } from "../context/CartContext";
 import CartItem from "../components/CartItem";
 import { useContext } from "react";
+import Footer from "../components/Footer";
 
 
 function Cart() {
   const { cart } = useContext(CartContext);
   let total = 0;
   for (let i = 0; i < cart.length; i++) {
-    total = total + cart[i].price * cart[i].quantity;
+    total = total + (cart[i].price * cart[i].quantity);
   }
   return (
     <div className="cart-page">
       <h1>Your Cart</h1>
       <div className="cart-container">
         <div>
-          {cart.length === 0 ? (
+          {cart.length === 0 ? 
             <p>No items in cart</p>
-          ) : (
+           : 
             cart.map((item) => (
               <CartItem key={item.id} item={item} />
             ))
-          )}
+          }
         </div>
         <div className="summary">
           <h2>Order Summary</h2>
